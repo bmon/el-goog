e7eb0dc8223b9cb3e1949e3a8d906debccaec060
Brendan Roy <br3ndanr@gmail.com>
2017-09-18 13:05:04 +1000

    add react hello world and required configs
    add package.json and lockfile which is used to maintain relevant versions of node modules
    add webpack.config.js which is used to configure webpack, the tool we use to build js files
    add assets dir, which contains /src/ and /static/
    add two jsx files to /assets/src/ which render a hello world message using react
    update readme partially
    update gopkg lockfile with new deps

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var config = {
+  context: __dirname + '/assets/src', // `__dirname` is root of project and `src` is source
+  entry: {
+    app: './index.jsx',
+  },
+  output: {
+    path: __dirname + '/assets/dist', // `dist` is the destination
+    publicPath: "/assets/",
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader',
+        query: {
+          presets:['env', 'react']
+        }
+      }, // to transform JSX into JS
+    ],
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: '../static', to: '.' }
+    ])
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
+
+module.exports = config;
