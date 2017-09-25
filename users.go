c60794f5ca57ace56a5d820790f648282dd690dc
Brendan Roy <br3ndanr@gmail.com>
2017-09-25 15:26:24 +1000

    add struct for user object

--- users.go
+++ users.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type User struct {
+	id       int
+	email    string
+	password string
+	verified bool
+}
+
 func UserCreate(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "123")
 }
