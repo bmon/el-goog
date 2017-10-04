bd2baa1f492ac3888a39ae458be8c6927522f46b
Michael Jefferies <michaeljefferies@Michaels-MacBook-Air.local>
2017-10-04 13:05:42 +1100

    Added in some test print lines, and regex-based checks to make sure email is valid

--- users.go
+++ users.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	//"database/sql"
+	"regexp"
 )
 
 type User struct {
@@ -13,5 +15,18 @@ type User struct {
 }
 
 func UserCreate(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "123")
+	email := r.PostFormValue("email")
+	password := r.PostFormValue("password")
+
+	fmt.Println("Here's the password so go stops complaining: "+password)
+	
+	var isEmail = regexp.MustCompile(`^.+\@.+\..+$`)
+
+	if(isEmail.MatchString(email)) {
+		fmt.Fprintf(w, "yeah mate is valid")
+        	fmt.Println("yeah mate is valid")
+	} else {
+		fmt.Fprintf(w, "yeah nah mate get a better email address")
+	        fmt.Println("yeah nah mate get a better email address")
+	}
 }
