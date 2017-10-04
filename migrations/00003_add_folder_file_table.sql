39a21ed0e954e6b5212d455ab80ec179f79bfbf5
Brendan Roy <br3ndanr@gmail.com>
2017-10-04 19:39:50 +1100

    add files and folders migration

---
+++ migrations/00003_add_folder_file_table.sql
@@ -0,0 +1,38 @@
+-- +goose Up
+CREATE TABLE folders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    parent_id INTEGER,
+    name TEXT NOT NULL,
+    modified INTEGER NOT NULL,
+    FOREIGN KEY(parent_id) REFERENCES folders(id)
+);
+
+CREATE TABLE files (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    parent_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    path TEXT NOT NULL,
+    size INTEGER NOT NULL,
+    checksum TEXT NOT NULL,
+    modified INTEGER NOT NULL,
+    FOREIGN KEY(parent_id) REFERENCES folders(id)
+);
+
+ALTER TABLE users ADD COLUMN root_folder INTEGER REFERENCES folders(id);
+
+
+
+-- +goose Down
+DROP TABLE folders;
+DROP TABLE files;
+
+-- no alter table drop column in sqlite :c
+ALTER TABLE users RENAME TO temp_users;
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+INSERT INTO users SELECT id, email, password FROM temp_users;
+DROP TABLE temp_users;
