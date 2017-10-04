16af7c44f780bb797e515d72d4617a944af683a5
Michael Jefferies <michaeljefferies@Michaels-MacBook-Air.local>
2017-10-04 15:20:22 +1100

    users now get added to the database during account creation

--- users.go
+++ users.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	//"database/sql"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 	"regexp"
+	"strconv"
 )
 
 type User struct {
@@ -15,16 +17,32 @@ type User struct {
 }
 
 func UserCreate(w http.ResponseWriter, r *http.Request) {
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
-	fmt.Println("Here's the password so go stops complaining: "+password)
+	eml := r.PostFormValue("email")
+	pwd := r.PostFormValue("password")
 	
 	var isEmail = regexp.MustCompile(`^.+\@.+\..+$`)
 
-	if(isEmail.MatchString(email)) {
+	if(isEmail.MatchString(eml)) {
 		fmt.Fprintf(w, "yeah mate is valid")
         	fmt.Println("yeah mate is valid")
+		db, err := sql.Open("sqlite3", "./elgoog.db")
+	        if  err != nil {
+			fmt.Println("couldn't open le database")
+		}
+		stmt, err := db.Prepare("INSERT INTO users(id, email, password) values(?,?,?)")
+	        if  err != nil {
+                        fmt.Println("insert line is broken, I dun goof'd") 
+                }
+		res, err := stmt.Exec(nil, eml, pwd)
+	        if  err != nil {
+                        fmt.Println("could not insert lel") 
+                }
+		id, err := res.LastInsertId()
+		if err !=nil {
+			fmt.Println("There was an insert errror")
+		} else {
+			fmt.Println("Result of SQL insert: "+strconv.FormatInt(id, 10))
+		}
 	} else {
 		fmt.Fprintf(w, "yeah nah mate get a better email address")
 	        fmt.Println("yeah nah mate get a better email address")
