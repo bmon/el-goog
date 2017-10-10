0260bb90366034b7312b18b778318396a433516e
Michael Jefferies <michaeljefferies@Michaels-MacBook-Air.local>
2017-10-10 15:30:53 +1100

    added in username to function for registering accounts

--- users.go
+++ users.go
@@ -19,21 +19,22 @@ type User struct {
 func UserCreate(w http.ResponseWriter, r *http.Request) {
 	eml := r.PostFormValue("email")
 	pwd := r.PostFormValue("password")
+        usr := r.PostFormValue("username")
 	
 	var isEmail = regexp.MustCompile(`^.+\@.+\..+$`)
 
 	if(isEmail.MatchString(eml)) {
-		fmt.Fprintf(w, "yeah mate is valid")
-        	fmt.Println("yeah mate is valid")
+		fmt.Fprintf(w, "yeah mate is valid email")
+        	
 		db, err := sql.Open("sqlite3", "./elgoog.db")
 	        if  err != nil {
 			fmt.Println("couldn't open le database")
 		}
-		stmt, err := db.Prepare("INSERT INTO users(id, email, password) values(?,?,?)")
+		stmt, err := db.Prepare("INSERT INTO users(id, email, password, username) values(?,?,?,?)")
 	        if  err != nil {
                         fmt.Println("insert line is broken, I dun goof'd") 
                 }
-		res, err := stmt.Exec(nil, eml, pwd)
+		res, err := stmt.Exec(nil, eml, pwd, usr)
 	        if  err != nil {
                         fmt.Println("could not insert lel") 
                 }
