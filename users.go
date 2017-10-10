2f460826bd3f54f5d707e5ad7c8a02efcc72fd98
Michael Jefferies <michaeljefferies@Michaels-MacBook-Air.local>
2017-10-10 15:59:50 +1100

    made users.go return more useful HTTP responses

--- users.go
+++ users.go
@@ -24,28 +24,31 @@ func UserCreate(w http.ResponseWriter, r *http.Request) {
 	var isEmail = regexp.MustCompile(`^.+\@.+\..+$`)
 
 	if(isEmail.MatchString(eml)) {
-		fmt.Fprintf(w, "yeah mate is valid email")
-        	
 		db, err := sql.Open("sqlite3", "./elgoog.db")
 	        if  err != nil {
+                        fmt.Fprintf(w, "ERROR: could not access database")
 			fmt.Println("couldn't open le database")
 		}
 		stmt, err := db.Prepare("INSERT INTO users(id, email, password, username) values(?,?,?,?)")
 	        if  err != nil {
+                        fmt.Fprintf(w, "ERROR: faulty SQL command")
                         fmt.Println("insert line is broken, I dun goof'd") 
                 }
 		res, err := stmt.Exec(nil, eml, pwd, usr)
 	        if  err != nil {
+                        fmt.Fprintf(w, "ERROR: Failed to execute SQL insert")
                         fmt.Println("could not insert lel") 
                 }
 		id, err := res.LastInsertId()
 		if err !=nil {
+                        fmt.Fprintf(w, "ERROR: failed to add data to database")
 			fmt.Println("There was an insert errror")
 		} else {
 			fmt.Println("Result of SQL insert: "+strconv.FormatInt(id, 10))
 		}
+                fmt.Fprintf(w, "Success! ID is:"+strconv.FormatInt(id,10))
 	} else {
-		fmt.Fprintf(w, "yeah nah mate get a better email address")
+		fmt.Fprintf(w, "ERROR: invalid email address")
 	        fmt.Println("yeah nah mate get a better email address")
 	}
 }
