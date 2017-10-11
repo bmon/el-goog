6f190edb867e6b0558c5df8eed4b05eb446ba0ef
david.sicong.zhou <iuyhcdfs@gmail.com>
2017-10-11 11:36:57 +1100

    fix up build ishes

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,10 @@
 # whether or not you actually run this, its a nice reminder
 # THIS KILLS THE DATABASE
+dep ensure
 rm elgoog.db
 goose -dir migrations/ sqlite3 ./elgoog.db up
-dep ensure
 go build
 npm install
 ./node_modules/.bin/webpack --config webpack.config.js
+echo "build finished, running el-goog"
 ./el-goog
