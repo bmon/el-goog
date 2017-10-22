4617f486315cf99d91edce4b07f76cd3f4f4a1f3
Brendan Roy <br3ndanr@gmail.com>
2017-10-22 23:30:36 +1100

    fug u

--- rebuild.sh
+++ rebuild.sh
@@ -7,6 +7,5 @@ goose -dir migrations/ sqlite3 ./elgoog.db up
 go build -i
 npm install
 ./node_modules/.bin/webpack --config webpack.config.js
-go build
 echo "build finished, running el-goog"
 ./el-goog
